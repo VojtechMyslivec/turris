85ab610a4171c03a60735a20958423655415f6ca
Vojtech Myslivec <vojtech@xmyslivec.cz>
2016-08-10 09:43:38 +0200

    LEDs: color change

--- diody/wi-fi_clients.sh
+++ diody/wi-fi_clients.sh
@@ -3,15 +3,15 @@
 ledMin=1
 ledMax=5
 ledPrefix=lan
+color="0088FF"
 
 client_count=$( iw dev wlan0 station dump | grep '^Station' | wc -l )
 
+rainbow "$ledPrefix" "$color" disable
 for i in $( seq "$ledMin" "$ledMax" ); do
     # universal formula
     if [ "$(( i - ledMin ))" -lt "$client_count" ]; then
         rainbow "${ledPrefix}${i}" enable
-    else
-        rainbow "${ledPrefix}${i}" disable
     fi
 done
 
